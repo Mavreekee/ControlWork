0c905b1b982fbbb61d5b09313bab89e38836bb87
Danila <xgreendaniel@gmail.com>
2022-07-30 12:37:31 +0500

    setting an array

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] mass = new string[size];
+for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите элемент массива {i}: ");
+        mass[i]  = Console.ReadLine();
+    }
